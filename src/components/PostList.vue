0685844ebf9853cb19bf0a3981f196d83935c0f1
旅者 <574008116@qq.com>
2023-04-12 23:33:41 +0800

    Changes

---
+++ src/components/PostList.vue
@@ -0,0 +1,38 @@
+<template>
+  <a-list item-layout="horizontal" :data-source="props.postList">
+    <template #renderItem="{ item }">
+      <a-list-item-meta>
+        <template #description>
+          <span v-html="item.highlight?.content || item.content"></span>
+        </template>
+        <template #title>
+          <a href="https://www.antdv.com/"
+            ><span v-html="item.highlight?.title || item.title"></span
+          ></a>
+        </template>
+        <template #avatar>
+          <a-avatar :src="gege" />
+        </template>
+      </a-list-item-meta>
+    </template>
+  </a-list>
+</template>
+
+<script setup lang="ts">
+import gege from "../assets/gege.jpg";
+import { withDefaults, defineProps } from "vue";
+
+interface Props {
+  postList: any[];
+}
+
+const props = withDefaults(defineProps<Props>(), {
+  postList: () => [],
+});
+</script>
+
+<style scoped>
+.gege {
+  width: 200px;
+}
+</style>
